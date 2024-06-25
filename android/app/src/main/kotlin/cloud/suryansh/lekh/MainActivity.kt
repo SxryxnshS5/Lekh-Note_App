590bb8c625cd35c4b187cc88da113134b28c9fc9
Suryansh <c2064746@newcastle.ac.uk>
2024-06-25 15:15:06 +0100

    initial commit

---
+++ android/app/src/main/kotlin/cloud/suryansh/lekh/MainActivity.kt
@@ -0,0 +1,5 @@
+package cloud.suryansh.lekh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lekh/firebase_options.dart';
+// ignore: unused_import
+import 'package:lekh/views/login_view.dart';
+// ignore: unused_import
+import 'package:lekh/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home', style:TextStyle(color: Colors.white)),
+      backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform,
+              ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user!.emailVerified) {
+              print('You are a verified user');
+            } else {
+              print('You need to verify your email');
+            }
+              return Text('Done');
+        default:
+          return const Text('Loading...');
+        }
+        },
+      ),
+    );
+  }
+}
+
+
